8b181f4e9896f6f57c6fa6f623284f5f995e6624
taka-engy <sake.sloth1110@gmail.com>
2023-08-13 18:10:10 +0900

    createThread.phpの作成

--- app/pages/createThread.php
+++ app/pages/createThread.php
@@ -18,5 +18,21 @@ include_once("../database/connect.php");
 <body>
   <?php include("../parts/header.php"); ?>
   <?php include("../parts/validation.php"); ?>
+
+  <div style="padding-left: 36px; color: blue;">
+    <h2 style="margin-top: 20px; margin-bottom: 0;">新規スレッド立ち上げ場</h2>
+  </div>
+  <form method="POST" class="formWrapper">
+    <div>
+      <label>スレッド名</label>
+      <input type="text" name="title">
+      <label>名前</label>
+      <input type="text" name="username">
+    </div>
+    <div>
+      <textarea name="submit" class="commentTextArea"></textarea>
+    </div>
+    <input type="submit" value="立ち上げ" name="commentTextArea">
+  </form>
 </body>
 </html>
