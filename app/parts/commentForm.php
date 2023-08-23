641ea9e8944a9fcc79762908106a0eeb0d5b8c61
taka-engy <sake.sloth1110@gmail.com>
2023-08-23 18:33:29 +0900

    commentFormの修正（エラー）

--- app/parts/commentForm.php
+++ app/parts/commentForm.php
@@ -1,3 +1,11 @@
+<?php
+$position = 0;
+
+if(isset($_POST["submitButton"])) {
+  $position = $_POST["position"];
+}
+?>
+
 <form class="formWrapper" method="POST" autocomplete="off">
   <div>
     <input type="submit" value="書き込む" name="submitButton">
@@ -8,4 +16,18 @@
   <div>
     <textarea class="commentTextArea" name="body"></textarea>
   </div>
+  <!-- 位置取得用 -->
+  <input type="hidden" name="position" value="0">
 </form>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+  // console.log($(window).scrollTop());
+  $(document).ready(() => {
+    $("input[type=submit]").click(() => {
+      // 現在のスクロール位置を取得
+      let position = $(window).scrollTop();
+      $("input:hidden[name=position]").val(position);
+    })
+    $(window).scrollTop(<?php echo $position; ?>);
+  })
+</script>
